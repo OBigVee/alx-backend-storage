c83803180ade3ea4d9900fca7c4c2c54252087f5
obigvee <adedejivictor98@gmail.com>
2024-01-29 13:37:52 +0100

    :task 0 completed :heavy_check_mark: - all are unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,3 @@
-/*
-script creates a table users with the following requirements id, integer, never null, auto increment and primary key
-email, string (255 characters), never null and unique name, string (255 characters)
-If the table already exists, your script should not fail
-Your script can be executed on any database
-*/
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
