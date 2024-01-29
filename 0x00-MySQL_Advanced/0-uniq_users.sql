8914453bb020b72dc8878c4c19c1206ec0baaaca
obigvee <adedejivictor98@gmail.com>
2024-01-29 13:40:22 +0100

    :task 0 completed :heavy_check_mark: - all are unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- createa a table with unique users.
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    email, VARCHAR(255) NOT NULL UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
