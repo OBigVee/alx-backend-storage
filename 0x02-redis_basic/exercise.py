78f769284b1501c06431168237526558c4addb66
obigvee <adedejivictor98@gmail.com>
2024-03-07 23:32:57 +0100

    write strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Optional, Union
+from typing import Optional, Union, Callable
 
 
 class Cache:
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """method generates a random key and stores the input
         data in Redis using the random key and return the key
         """
@@ -21,3 +21,27 @@ class Cache:
         self._redis.mset({key: data})
 
         return key
+    
+    def get(self, key:str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """ returns data of a key in a decoded form.
+        
+        key: str
+        fn: Callable argument
+        """
+        data = self._redis.get(key)
+
+        # conserve Redis.get behaviour if key does not exit
+        if fn is not None:
+            return fn(data)
+        return data
+
+    
+    def get_str(self, data : str) -> str:
+        """decode byte to str """
+        return data.decode("utf-8")
+    
+    def get_int(self, data: str) -> int:
+        """decode byte to int"""
+        return int(data)
+
+    
